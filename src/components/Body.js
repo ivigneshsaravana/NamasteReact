59ddd31822e48bb32ce437e16871e19d4c01efa8
Vignesh <Vignesh.s@excelindia.com>
2024-02-26 12:15:22 +0530

    Episode 6 completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,52 +1,82 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-const Body = () =>{
-    // Local State Variable - Super Powerfull Variable
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+const Body = () => {
+  // Local State Variable - Super Powerfull Variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const [filteredrestaurant, setFilteredRestaurant] = useState([]);
 
-    const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.314802421188112&lng=76.60672657191753&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const [searchText, setSearchText] = useState("");
+  console.log("Body rendered");
 
-        const json = await data.json();
-        console.log(json);
-        setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    if(listOfRestaurants.length === 0){
-        return <Shimmer />
-    }
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.3405386&lng=76.5748309&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
-    return (
-        <div className="body">
-            <div className="filter">
-                <button 
-                className="filter-btn" 
-                onClick={() =>{
-                    const filteredList = listOfRestaurants.filter(
-                        (res) => res.info.avgRating > 4.3
-                        );
-                    setListOfRestaurants(filteredList);
-                }
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
 
-                }
-                >Top Rated Restaurants</button>
-            </div>
-            <div className="res-container">
-                {
-                    listOfRestaurants.map( (restaurant) => (
-                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-                    ))
-                }
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
 
-            </div>
+  return (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={e => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // Filter the restaurent cards and update the UI
+              //searchText
+              const filteredRestaurant = listOfRestaurants.filter(
+                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
-}
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              res => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredrestaurant.map(restaurant => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
